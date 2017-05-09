dea862378ea9bb99d3db13a1a318e302e36d0385
uxxi <usmansbk@gmail.com>
2017-05-09 22:53:35 +0100

    'add andela_person.js'

--- oop/andela_person.js
+++ oop/andela_person.js
@@ -1,92 +1,63 @@
-/*
- * An abstract person class that 
- * that represents an instance of a person
- * at Andela.
- */
-function Person(name, email, gender) {
-	this.name = name;
-	this.email = email;
-	this.gender = gender;
-};
+class Person {
+	constructor(name, email, gender) {
+		this.name = name;
+		this.email = email;
+		this.gender = gender;
+	}
 
-Person.prototype = {
-	getName: function() {
+	getName() {
 		return this.name;
-	},
-	getGender: function() {
+	}
+
+	getGender() {
 		return this.gender;
-	},
-	getEmail: function() {
-		return this.gender;
-	},
-	toString: function() {
-		return `Name: ${this.name}, email: ${this.email}, gender: ${this.gender}`;
+	}
+
+	getEmail() {
+		return this.email;
+	}
+
+	toString() {
+		retur `Name: ${this.name}, email: ${this.email}, gender: ${this.gender};
 	}
 }
 
+class BFA extends Person {
+	constructor(name, email, gender, groupName) {
+		super(name, email, gender);
+		this.groupName = groupName;
+		this.group = [];
+	}
 
+	addGroupMember(camper) {
+		this.group.push(camper);
+		camper.setGroupName(this.groupName);
+		camper.assignBFA(this);
+	}
 
-/* BootcampFacilitatorAssistant (BFA) class
- * represents the detail of every bootcamp facilitator
- * assistant instance.
- */
-function BFA(name, email, gender, groupName) {
-	Person.call(this, name, email, gender);
-	this.groupName = groupName;
-	this.group = [];
-}
+	printGroupMembers() {
+		for (camper in this.group) {
+			console.log(camper.toString());
+		}
+	}
 
-BFA.prototype = new Person();
-BFA.prototype.constructor  = BFA;
-/*
- * Adds a bootcamper to the BFAs list of camper
- * and does all necessary initalization for the camper.
- */
-BFA.prototype.addGroupMember = function (camper) {
-	this.group.push(camper);
-	camper.setGroupName(this.groupName);
-	camper.setBFAName(this.name);
-}
-/*
- * print the detail of every member
- * in the BFAs list of campers.
- */
-BFA.prototype.printGroupMembers = function() {
-	for (camper in this.group) {
-		console.log(camper);
+	getGroupName() {
+		return this.groupName;
 	}
 }
-BFA.prototype.getGroupName = function() {
-	return this.groupName;
-}
 
-/*
- *This class represent the blueprint from
- * which bootcampers are initialized.
- */
-function BootCamper(name, email, gender) {
-	Person.call(this, name, email, gender);
-	this.groupName = undefined;
-	this.bfaName = undefined;
-};
+class Bootcamper extends Person {
+	constructor(name, email, gender) {
+		super(name, email, gender)
+		this.groupName = undefined;
+		this.bfa = undefined;
+	}
 
-BootCamper.prototype = new Person();
-BootCamper.prototype.constructor = BootCamper;
+	getBFA() {
+		return this.bfa;
+	}
 
-BootCamper.prototype.getGroupName = function () {
-	return this.groupName;
-}
-
-BootCamper.prototype.setGroupName = function (groupName) {
-	this.groupName = groupName;
-}
-
-// returns the name of the campers bootcamp facilitator assistant
-BootCamper.prototype.getBFAName = function () {
-	return this.bfa.getName();
-}
-
-// sets the name of the campers bootcamp facilitator assistant
-BootCamper.prototype.setBFAName = function(bfaName) {
-	this.bfaName = bfaName;
+	toString() {
+		return super.toString() + ", " + this.bfa.getName();
+	}
 }
