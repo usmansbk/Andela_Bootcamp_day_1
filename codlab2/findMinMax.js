4250e38f92ed62f31e13f206c30e2aed39842da5
Usman Suleiman <usmansbk@gmail.com>
2017-05-11 18:31:12 +0100

    update algorithm

--- codlab2/findMinMax.js
+++ codlab2/findMinMax.js
@@ -1,13 +1,13 @@
-function find(array, condition) {
-    let target = array[0]
-    for (let i = 1; i < array.length; i++) {
- 	if (condition(target, i))
-	    target = array[i];
-    	}
-    return min;
-}
-	
 function findMax (array) {
+    function find(array, condition) {
+        let target = array[0]
+        for (let i = 1; i < array.length; i++) {
+        if (condition(target, i))
+            target = array[i];
+        }
+        return target;
+    }
+    
     let minMaxArray = [];
     let minimum = find(array, (x, y)=>{return y < x;});
     let maximum = find(array, (x, y)=>{return y > x;});
@@ -17,6 +17,4 @@ function findMax (array) {
     }	
     return minMaxArray;
 }
-
-module.exports.findMax = findMax;
-module.exports.find = find;
+module.exports = findMax; 
