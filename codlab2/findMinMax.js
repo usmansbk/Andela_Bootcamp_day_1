484e2bcb13ac99aabebb521367d4964f6cefd65e
Usman Suleiman <usmansbk@gmail.com>
2017-05-11 18:36:46 +0100

    fix bug

--- codlab2/findMinMax.js
+++ codlab2/findMinMax.js
@@ -2,15 +2,15 @@ function findMax (array) {
     function find(array, condition) {
         let target = array[0]
         for (let i = 1; i < array.length; i++) {
-        if (condition(target, i))
+        if (condition(array[i], target))
             target = array[i];
         }
         return target;
     }
     
     let minMaxArray = [];
-    let minimum = find(array, (x, y)=>{return y < x;});
-    let maximum = find(array, (x, y)=>{return y > x;});
+    let minimum = find(array, (x, y)=>{return x < y;});
+    let maximum = find(array, (x, y)=>{return x > y;});
     minMaxArray.push(minimum);
     if (minimum !== maximum) {
         minMaxArray.push(maximum);
