a966290ce1bd848e57c83cfc949c6fe55bacdef1
Usman Suleiman <usmansbk@gmail.com>
2017-05-11 18:31:58 +0100

    updata algo

--- codlab2/spec/findMinMaxSpec.js
+++ codlab2/spec/findMinMaxSpec.js
@@ -4,7 +4,6 @@ const findMinMax = require("../findMinMax.js");
   describe('Min-Max Numbers in a List: ', function () {
 
     describe('Return the min and max number in the list in a new list follows `[min, max]`', function () {
-
       it('should return [1,4] for [1, 2, 3 , 4]', function () {
         expect(findMinMax([1, 2, 3, 4])).toEqual([1, 4]);
       });
